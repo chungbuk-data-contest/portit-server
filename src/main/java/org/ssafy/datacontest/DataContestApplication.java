d4ca79571bec9963f2652a1696aac0a1c4409443
홍시은 <sieun4449@gmail.com>
2025-06-15 21:22:27 +0900

    setting

--- src/main/java/org/ssafy/datacontest/DataContestApplication.java
+++ src/main/java/org/ssafy/datacontest/DataContestApplication.java
@@ -2,8 +2,9 @@ package org.ssafy.datacontest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DataContestApplication {
 
     public static void main(String[] args) {
