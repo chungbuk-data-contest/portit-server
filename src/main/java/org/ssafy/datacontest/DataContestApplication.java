e95cde6f4139684a7f642b2375644d67caee031a
홍시은 <sieun4449@gmail.com>
2025-06-15 16:54:14 +0900

    Initial commit

---
+++ src/main/java/org/ssafy/datacontest/DataContestApplication.java
@@ -0,0 +1,13 @@
+package org.ssafy.datacontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataContestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataContestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ssafy/datacontest/DataContestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.datacontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataContestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
