20f62a2d9a4aaf398852c9c8257948a58c8b3a7f
홍시은 <tjwns53@naver.com>
2025-06-19 17:22:32 +0900

    docs: 작품 등록 swagger 수정

--- src/main/java/org/ssafy/datacontest/controller/ArticleController.java
+++ src/main/java/org/ssafy/datacontest/controller/ArticleController.java
@@ -1,12 +1,12 @@
 package org.ssafy.datacontest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +25,10 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @PostMapping("") // 객체 보내주기
+    @Operation(
+            summary = "작품 등록",
+            description = "작품을 등록하고 작품 번호를 리턴합니다."
+    )
     public ResponseEntity<?> registerArticle(@ModelAttribute @Valid ArticleRequestDto request) throws CustomException {
 
         Long articleId = articleService.createArticle(request);
