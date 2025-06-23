e5a62d4da602c3433a81a10a32b9a90b45996252
dongri <ehdrms001030@naver.com>
2025-06-24 02:10:35 +0900

    feat: fcm token 받을 수 있는 api 구현

--- src/main/java/org/ssafy/datacontest/controller/FcmController.java
+++ src/main/java/org/ssafy/datacontest/controller/FcmController.java
@@ -2,6 +2,7 @@ package org.ssafy.datacontest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,10 +23,16 @@ public class FcmController {
         this.fcmService = fcmService;
     }
 
-    @PostMapping("/send")
+    @PostMapping("/token")
     public ResponseEntity<Void> registerFcmToken(@RequestBody FcmTokenRequest fcmTokenRequest,
                                                  @AuthenticationPrincipal CustomUserDetails userDetails) {
-        fcmService.saveOrUpdateToken(fcmTokenRequest, userDetails.getUsername());
+        // ROLE_USER, ROLE_COMPANY 중 하나만 있다고 가정할 때
+        String role = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority) // "ROLE_USER" 등
+                .findFirst()                      // 여러 개 중 첫 번째
+                .orElse("ROLE_USER");             // 없을 경우 기본값
+
+        fcmService.saveOrUpdateToken(fcmTokenRequest, userDetails.getUsername(), role);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/ssafy/datacontest/service/FcmService.java
+++ src/main/java/org/ssafy/datacontest/service/FcmService.java
@@ -3,5 +3,5 @@ package org.ssafy.datacontest.service;
 import org.ssafy.datacontest.dto.fcm.FcmTokenRequest;
 
 public interface FcmService {
-    void saveOrUpdateToken(FcmTokenRequest fcmTokenRequest, String username);
+    void saveOrUpdateToken(FcmTokenRequest fcmTokenRequest, String username, String role);
 }
