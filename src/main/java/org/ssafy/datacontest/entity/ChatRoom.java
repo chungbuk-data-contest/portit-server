3a3e202230d3f4440acb106dfdb897ec05693128
dongri <ehdrms001030@naver.com>
2025-06-25 02:44:35 +0900

    feat: ChatRoom 엔티티 설계 #11

--- src/main/java/org/ssafy/datacontest/entity/ChatRoom.java
+++ src/main/java/org/ssafy/datacontest/entity/ChatRoom.java
@@ -1,13 +1,31 @@
 package org.ssafy.datacontest.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String roomId; // 채팅방 ID (UUID 등으로 생성)
+
+    // 어떤 게시글(article)을 주제로 한 대화방인지
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
     private Article article;
+
+    // 대화하는 일반 사용자 (학생)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 대화하는 기업
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
 }
