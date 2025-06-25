909df5f87307e7eeae23c69d6c8064702ff1defb
dongri <ehdrms001030@naver.com>
2025-06-26 02:18:58 +0900

    feat: SpringBootApplication 실행 시 Timezone을 Seoul로

--- src/main/java/org/ssafy/datacontest/DataContestApplication.java
+++ src/main/java/org/ssafy/datacontest/DataContestApplication.java
@@ -1,9 +1,12 @@
 package org.ssafy.datacontest;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EntityScan(basePackages = "org.ssafy.datacontest.entity")
@@ -13,4 +16,8 @@ public class DataContestApplication {
         SpringApplication.run(DataContestApplication.class, args);
     }
 
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
